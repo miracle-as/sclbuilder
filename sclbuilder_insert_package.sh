06762dc93b0e8161964935762339e7f3c06e5b13
Jakob Holst <jho@miracle.dk>
2021-12-02 07:57:47 +0100

    upload execute bit set

--- sclbuilder_insert_package.sh
+++ sclbuilder_insert_package.sh
@@ -1,35 +1,8 @@
 #!/usr/bin/env bash
 
 URL=$1
-FILENAME="/tmp/upload.requirement.txt"
-curl $URL > $FILENAME
-if [[ $? != 0 ]]
-then
-    echo "`date`: File not found"
-    rm $FILENAME
-    exit 1
-fi 
 
-
-if [[ ! -f $FILENAME ]];
-then
-    echo "`date`: File not found"
-    rm $FILENAME
-    exit 1
-fi 
-
-
-grep "==" $FILENAME >/dev/null 2>&1
-if [[ $? != 0 ]];
-then
-    echo "`date`: File does not look like at req file"
-    rm $FILENAME
-    exit 1
-fi 
-
-
-
-for REQ in `cat $1 |tr -d ' '|tr -d '\r'`
+for REQ in `cat $URL |tr -d ' '|tr -d '\r'`
 do
     echo $REQ
 done
