09c3ffc81a48f03cd38f656f052fdae64fd88e25
Jakob Holst <jho@miracle.dk>
2021-12-02 18:22:43 +0100

    scripts to build

--- sclbuilder_insert_package.sh
+++ sclbuilder_insert_package.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 URL=$1
-
+SCRIPT=/tmp/packagers.sh
+echo > $SCRIPT
 for REQ in `curl $URL |tr -d ' '|tr -d '\r'`
 do
     NAME=`echo $REQ | awk -F'==' '{ print $1 }'`
     VERS=`echo $REQ | awk -F'==' '{ print $2 }'`
     SLUG=`echo ${NAME}___${VERS} | tr '.' '_'`
     EXTRAVAR="'{ \"slug\": \"$SLUG\", \"name\": \"$NAME\", \"version\": \"$VERS\" }'"
-    ansible-playbook -i inventory sclbuilder_insert_package.yml --extra-vars $EXTRAVAR
+    echo ansible-playbook -i inventory sclbuilder_insert_package.yml --extra-vars $EXTRAVAR >> $SCRIPT
 done
+
+
+bash $SCRIPT
