c69901e56c618db7435d24815e28b2831e537673
Jakob Holst <jho@miracle.dk>
2021-12-02 08:05:45 +0100

    upload execute bit set

--- sclbuilder_insert_package.sh
+++ sclbuilder_insert_package.sh
@@ -6,5 +6,6 @@ for REQ in `curl $URL |tr -d ' '|tr -d '\r'`
 do
     NAME=`echo $REQ | awk -F'==' '{ print $1 }'`
     VERS=`echo $REQ | awk -F'==' '{ print $2 }'`
-    ansible-playbook -i inventory sclbuilder_insert_package.yml --extra-vars {' "slug": "$REQ", "name": "$NAME", "version": "$VERS" }'
+    EXTRAVAR="'{ \"slug\": \"$REQ\", \"name\": \"$NAME\", \"version\": \"$VERS\" }'"
+    echo ansible-playbook -i inventory sclbuilder_insert_package.yml --extra-vars $EXTRAVAR
 done
